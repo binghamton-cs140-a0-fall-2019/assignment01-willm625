a5fcffbf741cacc5014719ee09f418303669b07f
William Cornejo <wcornej1@binghamton.edu>
2019-08-29 19:05:31 -0400

    new new commit

--- assignment01/SimpleDateTester.java
+++ assignment01/SimpleDateTester.java
@@ -6,11 +6,10 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
 public class SimpleDateTester{
-    static SimpleDate datea = SimpleDate.of(2000, 4, 1);
-    static  SimpleDate dateb = SimpleDate.of(2001, 3, 4);
-    static SimpleDate datea2 = SimpleDate.of(2000, 4, 1);
-    static  SimpleDate dateb2 = SimpleDate.of(2001, 3, 4);
-    public static void main(String[] args){  
+
+    public static void main(String[] args){
+	  SimpleDate datea = SimpleDate.of(2000, 4, 1);
+          SimpleDate dateb = SimpleDate.of(2001, 3, 4);
           System.out.println(datea.before(dateb));
 
 	  try(var output =new PrintWriter(new FileOutputStream(
@@ -20,6 +19,8 @@ public class SimpleDateTester{
 		//Copy all your lines above, add an extra "2" to every variable name
 		// and replace every System.out.print or System.out.println
       		// by output.print or output.println
+	  SimpleDate datea2 = SimpleDate.of(2000, 4, 1);
+          SimpleDate dateb2 = SimpleDate.of(2001, 3, 4);
 
 			output.println(datea2.before(dateb2));		
 		} catch (FileNotFoundException e) {
